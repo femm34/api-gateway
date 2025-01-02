6b514d54697ba653c6ea155dd022f6543107ad27
Fernando Emanuel <78227397+femm34@users.noreply.github.com>
2025-01-01 18:23:11 -0600

    Initial commit: Uploading api gateway

---
+++ src/test/java/com/fecd/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fecd.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
